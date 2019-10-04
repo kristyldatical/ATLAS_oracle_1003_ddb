e88353ee87406696674166637cb76b9de90db41b
kristyldatical <kgomes@datical.com>
2019-10-04 19:42:33 +0000

    updates

---
+++ Resources/sql_scripts/20191004192010_my_table.sql
@@ -0,0 +1,29 @@
+SET ECHO ON
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+spool '20191004192010_my_table.log'
+
+set define off
+
+SET SQLBLANKLINES ON
+
+SET ERRORLOGGING ON 
+
+SET ERRORLOGGING ON IDENTIFIER '6dfd1025-98e9-4f09-b8c9-c935e0296b5f'
+set linesize 1000;
+SELECT sys_context('userenv','server_host') as DATICAL_TRACEFILE_HOSTNAME from dual;
+alter session set timed_statistics = true;
+alter session set tracefile_identifier =  '20191004192010754';
+alter session set max_dump_file_size = unlimited;
+alter session set events '10046 trace name context forever, level 1';
+SELECT value as DATICAL_TRACE_FILE FROM v$diag_info WHERE name = 'Default Trace File';
+exec DBMS_APPLICATION_INFO.SET_CLIENT_INFO ('Datical Client');
+ALTER SESSION SET CURRENT_SCHEMA = intuser;
+ALTER SESSION SET CURSOR_SHARING = EXACT;
+create table ATLAS_SCHEMA_A.my_table_3(id int);
+alter session set events '10046 trace name context off';
+
+set define on
+
+spool off
+
+exit
